5ea7866d94a18bf3b586a8422c9552dd05ad704b
bxshi <baoxu.shi@gmail.com>
2013-12-03 13:47:10 -0500

    specify disk/memory R/W time

--- src/main/java/edu/nd/bshi/scheduler/Disk.java
+++ src/main/java/edu/nd/bshi/scheduler/Disk.java
@@ -3,6 +3,7 @@ package edu.nd.bshi.scheduler;
 public class Disk {
     private int lastLocation;
     private int seekTime;
+    private static final int DISK_TIME = 4100000;
 
     public Disk(Integer seek_time){
         this.lastLocation = 0;
@@ -14,13 +15,13 @@ public class Disk {
         if (seekDistance!=0)
             event.addTime(seekTime);
         this.lastLocation = event.getBaseAddress()+event.getAddressLength();
-        event.addTime(2);
+        event.addTime(DISK_TIME);
     }
     public void write(Event event){
         int seekDistance = Math.abs(this.lastLocation-event.getBaseAddress());
         if (seekDistance!=0)
             event.addTime(seekTime);
         this.lastLocation = event.getBaseAddress()+event.getAddressLength();
-        event.addTime(3);
+        event.addTime(DISK_TIME);
     }
 }
--- src/main/java/edu/nd/bshi/scheduler/Memory.java
+++ src/main/java/edu/nd/bshi/scheduler/Memory.java
@@ -1,30 +1,27 @@
 package edu.nd.bshi.scheduler;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Memory {
-    private int virtualMemSize, physicalMemSize;
-    private int blockLoadTime = 10;
-    private int blockWriteTime = 10;
-    private int blockInMemoryRead = 1;
-    private int blockInMemoryWrite = 1;
+    private int blockLoadTime = 4100000;
+    private int blockWriteTime = 4100000;
+    private int blockInMemoryRead = 25;
+    private int blockInMemoryWrite = 25;
     private HashMap<Integer, Integer> memorySpace = new HashMap<Integer, Integer>();
     private HashMap<Integer, Integer> LRU = new HashMap<Integer, Integer>();
     private static final int IN_MEM = 1;
     private static final int OUT_MEM = 0;
     private static final int DIRTY = 2;
+    private boolean hasLRU = true;
 
-    public Memory(int virtualMemSize, int physicalMemSize) {
-        this.virtualMemSize = virtualMemSize;
-        this.physicalMemSize = physicalMemSize;
-
+    public Memory(int virtualMemSize, int physicalMemSize, boolean hasLRU) {
+        this.hasLRU = hasLRU;
         //initialize memory
         for(int i = 0; i < physicalMemSize; i++) {
             memorySpace.put(i, IN_MEM);
-            HashMap<Integer, Integer> memoryBlk = new HashMap<Integer, Integer>();
-            LRU.put(i, 0);
+            if(this.hasLRU)
+                LRU.put(i, 0);
         }
         for(int i = physicalMemSize; i < virtualMemSize; i++) {
             memorySpace.put(i, OUT_MEM);
@@ -34,29 +31,40 @@ public class Memory {
     private int loadMemory(int blk){
         int time = 0;
         time += this.removeMemory();
-        this.LRU.put(blk, 0);
-        return time + this.memorySpace.put(blk, IN_MEM);
+        if(this.hasLRU)
+            this.LRU.put(blk, 0);
+        this.memorySpace.put(blk, IN_MEM);
+        return time;
     }
 
     private int removeMemory(){
         int lowest = 0;
         int hit = 9999;
         int time = 0;
-
-        for(int i : this.LRU.keySet()){
-            if(this.LRU.get(i)<=hit){
-                lowest = i;
-                hit = this.LRU.get(i);
+        if(this.hasLRU){
+            for(int i : this.LRU.keySet()){
+                if(this.LRU.get(i)<=hit){
+                    lowest = i;
+                    hit = this.LRU.get(i);
+                }
             }
-        }
 
-        if(this.memorySpace.get(lowest) == DIRTY){
-            time += this.blockWriteTime;
-        }
-        this.memorySpace.put(lowest, OUT_MEM);
-        this.LRU.remove(lowest);
+            if(this.memorySpace.get(lowest) == DIRTY){
+                time += this.blockWriteTime;
+            }
+            this.memorySpace.put(lowest, OUT_MEM);
+            this.LRU.remove(lowest);
 
-        return time + this.blockLoadTime;
+            return time + this.blockLoadTime;
+        }else{
+            int rdm = (new Random().nextInt() % this.memorySpace.keySet().size());
+            while(this.memorySpace.get(rdm)==null || this.memorySpace.get(rdm)==OUT_MEM)
+                rdm = (new Random().nextInt() % this.memorySpace.keySet().size());
+            if(this.memorySpace.get(rdm) == DIRTY)
+                time += this.blockWriteTime;
+            this.memorySpace.put(rdm, OUT_MEM);
+            return time + this.blockLoadTime;
+        }
     }
 
     public void read(Event event) {
@@ -65,18 +73,21 @@ public class Memory {
             for(int i = event.getBaseAddress(); i < event.getBaseAddress()+event.getAddressLength(); i++) {
                 if(memorySpace.get(i) == OUT_MEM){
                     //TODO not in memory, need to do a disk load
-                    time += this.loadMemory(i);
+                    int t = this.loadMemory(i);
+                    if(t > time) time = t;
                 }else{
-                    int hit = LRU.get(i);
-                    LRU.put(i, hit+1);
-                    time += blockInMemoryRead;
+                    if(this.hasLRU){
+                        int hit = LRU.get(i);
+                        LRU.put(i, hit+1);
+                    }
+
                 }
             }
         }catch (Exception err){
             err.printStackTrace();
         }
 
-        event.addTime(time);
+        event.addTime(time+blockInMemoryRead);
 
     }
 
@@ -86,12 +97,14 @@ public class Memory {
             for(int i = event.getBaseAddress(); i < event.getBaseAddress()+event.getAddressLength(); i++) {
                 if(memorySpace.get(i) == OUT_MEM){
                     //TODO not in memory, need to do a disk load
-                    time += this.loadMemory(i);
+                    int t = this.loadMemory(i);
                     memorySpace.put(i, DIRTY);
+                    if(t > time) time = t;
                 }else{
-                    int hit = LRU.get(i);
-                    LRU.put(i, hit+1);
-                    time += blockInMemoryWrite;
+                    if(this.hasLRU){
+                        int hit = LRU.get(i);
+                        LRU.put(i, hit+1);
+                    }
                     memorySpace.put(i, DIRTY);
                 }
             }
@@ -99,7 +112,7 @@ public class Memory {
             err.printStackTrace();
         }
 
-        event.addTime(time);
+        event.addTime(time+blockInMemoryWrite);
     }
 
 }
